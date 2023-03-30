f5be7c1929e0e5bb3f4632141c062e95c889aed3
ChihebLhb <chiheb.lahbib@esen.tn>
2023-03-30 02:44:18 +0100

    chiheb

---
+++ src/main/java/com/example/MsGestionAnnApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionAnnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsGestionAnnApplication.class, args);
+    }
+
+}
